88ed0bf94465feb1833f7e58d9ce9b86edf91022
mariaarnesson <maria_arnesson@outlook.com>
2023-02-08 09:57:36 +0000

    Change css source code to wrapper

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
-const cards = document.querySelectorAll('.memory-card');
 const moves = document.getElementById("moves-count");
 const timeValue = document.getElementById("time");
 const startButton = document.getElementById("start");
 const stopButton = document.getElementById("stop");
+const gameContainer = document.querySelector(".game-container");
 const result = document.getElementById("result");
 const controls = document.querySelector(".controls-container");
-
-let hasFlippedCard = false;
-let lockBoard = false;
-let firstCard, secondCard;
+let cards;
+let interval;
+let firstCard = false;
+let secondCard = false;
 
 //Items array
 
