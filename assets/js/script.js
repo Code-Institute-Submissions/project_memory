205535e7f07fb2a697e01fc7e80e363195db1390
mariaarnesson <maria_arnesson@outlook.com>
2023-02-11 15:26:57 +0000

    Add functioner to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+const moves = document.getElementById("number_of_moves");
+const timeValue = document.getElementById("gameTime");
+const startButton = document.getElementById("start");
+
+function gameInstruction() {}
+
+function timeGenerator() 
+
+function generateRandom() 
+
+function matrixGenerator()
+
+function startButton() 
+
+function stopButton() 
+
+function initializer() 
