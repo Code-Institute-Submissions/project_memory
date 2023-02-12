47b03461651cae8b78b57ceae58160cf507d128f
mariaarnesson <maria_arnesson@outlook.com>
2023-02-12 20:52:17 +0000

    Add all images to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,17 @@ function gameInstruction() {
     }
   }
 
+  const items = [
+    { name: "be_happy", image: "assets/images/be_happy.jpg" },
+    { name: "be_smart", image: "assets/images/be_smart.jpg" },
+    { name: "be_smile", image: "assets/images/be_smile.jpg" },
+    { name: "hey_world", image: "assets/images/hey_world.jpg" },
+    { name: "love_snowie", image: "assets/images/love_snowie.jpg" },
+    { name: "nice_kids", image: "assets/images/nice_kids.jpg" },
+    { name: "piggypig", image: "assets/images/piggypig.jpg" },
+    { name: "beauty", image: "assets/images/beauty.jpg" },
+  ]
+
 
 function timeGenerator() 
 
