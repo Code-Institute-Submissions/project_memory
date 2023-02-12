7d3791fcbf60eb1dae278e5bd2c08e06efaeda21
mariaarnesson <maria_arnesson@outlook.com>
2023-02-12 20:37:22 +0000

    Add content to function gameInstruction

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,15 @@ const moves = document.getElementById("number_of_moves");
 const timeValue = document.getElementById("gameTime");
 const startButton = document.getElementById("start");
 
-function gameInstruction() {}
+function gameInstruction() {
+    var x = document.getElementById("instruction");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+  }
+
 
 function timeGenerator() 
 
